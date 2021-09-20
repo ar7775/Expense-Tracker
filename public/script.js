4b1feee3b7b09b8620602f489499b40ebdb4f827
Aryan Raj <ar7775@srmist.edu.in>
2021-09-20 19:32:55 +0530

    ready to deploy

---
+++ public/script.js
@@ -0,0 +1,134 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+
+var dps = []; // dataPoints
+var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    title: {
+        text: "Expenses"
+    },
+    data: [{
+        type: "pie",
+        showInLegend: true,
+        startAngle: 270,
+        toolTipContent: "{y} - #percent %",
+        yValueFormatString: "₹#####.00",
+        indexLabel: "{label}",
+        legendText: "{label}",
+        dataPoints: dps
+    }]
+});
+
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
+
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        updateChart(transaction);
+        chart.render();
+
+        updateLocalStorage();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
+function addTransactionDOM(transaction) {
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount)}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id
+    })">x</button>`;
+
+    list.appendChild(item);
+}
+
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+
+    balance.innerText = `₹${total}`;
+    money_plus.innerText = `₹${income}`;
+    money_minus.innerText = `₹${expense}`;
+}
+
+function updateChart(transaction) {
+    if (transaction.amount < 0)
+        dps.push({
+            label: transaction.text,
+            y: Math.abs(transaction.amount)
+        });
+}
+
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    updateLocalStorage();
+
+    dps.splice(0, dps.length);
+
+    init();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init() {
+    list.innerHTML = '';
+
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+    transactions.forEach(updateChart);
+    chart.render();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
